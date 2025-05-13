5c2577ffb54198ab7c2919b0be1c4b7e2fd03297
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-05-13 10:17:44 -0700

    pepe

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-resultado = a + b
-
-print(f"{a} + {b} = {resultado}")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    resultado = a + b
+    print(f"{a} + {b} = {resultado}")
