f0933f05af74aad003a63ecc9d6cfd7170216dba
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-05-19 05:54:12 -0700

    pepe

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for number in my_list:
+        if not isinstance(number, int):
+            raise TypeError("All elements must be integers")
         print("{}" .format(number))
