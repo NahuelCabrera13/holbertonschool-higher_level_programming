dfe8c056b8255ba820b4a8475b813d11e16e0baf
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-05-25 18:26:34 -0700

    pepe

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Este módulo define una clase vacía Square que representa un cuadrado."""
+"""Este módulo define una clase"""
 class Square:
     """Esta es una clase vacía que define un cuadrado."""
     pass
