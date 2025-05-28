f7b3adf6d806875db78d478303ba31d6007df50c
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-05-28 09:43:14 -0700

    pepe

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -45,5 +45,5 @@ class Rectangle:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
-    def ___repr___(self):
-        return "Rectangle({}, {})" .format(self.__width, self.__height)
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
