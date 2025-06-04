2734ecdcca9ca171f4cb73c9b9f701cca49b246a
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-04 14:53:07 -0700

    pepe

--- python-inheritance/8-main.py
+++ python-inheritance/8-main.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""pepe"""
+Rectangle = __import__('8-rectangle').Rectangle
 
-class BaseGeometry:
-    """Base class for geometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
+r = Rectangle(3, 5)
 
-    def integer_validator(self, name, value):
-        if isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+print(r)
+print(dir(r))
 
-class Rectangle(BaseGeometry):
-    """Rectangle that inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-print(issubclass(Rectangle, BaseGeometry))
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,45 +1,11 @@
 #!/usr/bin/python3
-"""pepe"""
+"""Rectangle class"""
 
-
-class BaseGeometry:
-    """pepe"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """pepe"""
-        
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        #!/usr/bin/python3
-"""pepe"""
-class BaseGeometry:
-    """pepe"""
-    def area(self):
-        raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
-        if isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-class Rectangle(BaseGeometry):
-    """pepe"""
-        
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.width = width
-        self.height = height 
+        self.__width = width
+        self.__height = height
