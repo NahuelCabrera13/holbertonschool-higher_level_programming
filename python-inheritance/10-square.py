3f8b3b427f20e11cdddbbb890a3a26aa6bbb057a
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-04 15:20:59 -0700

    pepe

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,6 +5,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """pepe"""
+    
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
