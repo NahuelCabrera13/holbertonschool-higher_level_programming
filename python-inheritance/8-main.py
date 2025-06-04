ea2d03534c73440e04698aa3ebea9e6fecb26d33
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-04 14:47:28 -0700

    pepe

--- python-inheritance/8-main.py
+++ python-inheritance/8-main.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-Rectangle = __import__('8-rectangle').Rectangle
+"""pepe"""
 
-r = Rectangle(3, 5)
+class BaseGeometry:
+    """Base class for geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
 
-print(r)
-print(dir(r))
+    def integer_validator(self, name, value):
+        if isinstance(value, bool):
+            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+class Rectangle(BaseGeometry):
+    """Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+print(issubclass(Rectangle, BaseGeometry))
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -22,5 +22,24 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height 
+        #!/usr/bin/python3
+"""pepe"""
+class BaseGeometry:
+    """pepe"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if isinstance(value, bool):
+            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+class Rectangle(BaseGeometry):
+    """pepe"""
+        
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.width = width
+        self.height = height 
