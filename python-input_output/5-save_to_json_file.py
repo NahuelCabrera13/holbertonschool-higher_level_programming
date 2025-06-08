2a95938c5f957b8f04b19e089c5cb4e5d363aace
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-07 21:13:53 -0700

    pepe

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """pepe"""
-
     with open(filename, "w", encoding="utf-8") as  pepe:
-        return json.dump(my_obj, pepe)
+        json.dump(my_obj, pepe)
