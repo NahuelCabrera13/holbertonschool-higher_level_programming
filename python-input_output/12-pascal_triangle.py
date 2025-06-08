b86f564ae8ddc71f1de0eda6ca25a4fcc0c03eaf
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-08 11:25:37 -0700

    pepe

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,7 +3,7 @@
 
 
 def pascal_triangle(n):
-    """pepe"""
+   """pepe"""
 
     if n <= 0:
         return []
@@ -11,13 +11,12 @@ def pascal_triangle(n):
     triangle = [[1]]
 
     for i in range(1, n):
-
         prev_row = triangle[-1]
 
         current_row = [1]
 
-    for j in range(1, len(prev_row)):
-        current_row.append(prev_row[j-1] + prev_row[j])
+        for j in range(1, i):
+            current_row.append(prev_row[j-1] + prev_row[j])
 
         current_row.append(1)
 
