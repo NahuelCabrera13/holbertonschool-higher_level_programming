5538e20e122dba9f74343b16b441adef40cefca7
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-08 09:45:04 -0700

    pepe

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """pepe"""
-import json
 
 
 class Student():
+    """pepe"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
