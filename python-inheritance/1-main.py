7389fc563f0942dff00d9acc4ceecf88e4fd5565
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-09 06:37:09 -0700

    pepe

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+search_replace = __import__('1-search_replace').search_replace
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
 print(my_list)
