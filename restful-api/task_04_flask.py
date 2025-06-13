6677afc139ac9e05fb30943ef49a1e6af6c21496
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-13 05:30:44 -0700

    pepe

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,41 +1,58 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+users = users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+}
 
 app = Flask(__name__)
 
-usuarios = {} 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Welcome to the User API!', 200
+    return("Welcome to the Flask API!")
 
-@app.route('/data', methods=['GET'])
+@app.route("/data")
 def data():
-    return jsonify(list(usuarios.values())), 200
+    usernames = list(users.keys())
+    return jsonify(usernames)
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<usernames>")
+def get_users(usernames):
+    user = users.get(usernames)
+
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"Error": "User not found"}), 404
+
+
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
-
     if not data or 'username' not in data:
-        return jsonify({'error': 'Username is required'}), 400
-
+        return jsonify({"error": "Username is required"}), 400
     username = data['username']
 
-    if username in usuarios:
-        return jsonify({'error': 'Username already exists'}), 400
+    users[username] = {
+        "username": username,
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+    
+    return jsonify({
+        "message": "User  added",
+        "user": users[username]
+    }), 201
 
-    usuarios[username] = data
-    return jsonify({'message': 'User added successfully'}), 201
 
-@app.route('/users/<username>', methods=['GET'])
-def get_user(username):
-    if username in usuarios:
-        return jsonify(usuarios[username]), 200
-    else:
-        return jsonify({'error': 'User not found'}), 404
 
-@app.route('/status', methods=['GET'])
-def status():
-    return jsonify({'status': 'OK'}), 200
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
