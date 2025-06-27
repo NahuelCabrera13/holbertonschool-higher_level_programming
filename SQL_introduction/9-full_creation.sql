6f10b62a9d4101764355549fa61d83338089cd24
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-27 11:10:26 -0300

    pepe

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- this 
 CREATE TABLE if not exists second_table(id int AUTO_INCREMENT PRIMARY KEY,name varchar(256), score int);
-INSERT INTO second_table(name, score) VALUES('Jhon', 10);
+INSERT INTO second_table(name, score) VALUES('John', 10);
 INSERT INTO second_table(name, score) VALUES('Alex', 3);
 INSERT INTO second_table(name, score) VALUES('Bob', 14);
 INSERT INTO second_table(name, score) VALUES('George', 8);
