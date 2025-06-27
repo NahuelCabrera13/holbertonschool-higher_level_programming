8b2c603bcc30c0c3c232d77cd87bc4858c23da99
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-27 09:48:28 -0300

    pepe

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- This scrip create a table whit determinted chracacters
-create table if exists first_table(id int, name varchar(256))
+create table if not exists first_table(id int, name varchar(256))
