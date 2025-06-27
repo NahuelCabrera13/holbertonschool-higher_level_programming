aacd59c22e9a4954b45d3edb2a302bc37bc6675a
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-06-27 10:57:05 -0300

    pepe

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- this 
+CREATE TABLE if not exists second_table(id int AUTO_INCREMENT,name varchar(256), score int)hnhjhnbm bnj
+INSERT INTO second_table(name, secore) VALUES('JHON' 10)
+INSERT INTO second_table(name, score) VALUES('Alex' 3)
+INSERT INTO second_table(name, score) VALUES('Bob' 14)
+INSERT INTO second_table(name, score) VALUES('George' 8)clear
