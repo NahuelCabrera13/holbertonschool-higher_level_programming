b368daa1f0304b1b0b78809f21f7e8e656403516
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-07-11 10:46:43 -0300

    pepe

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const String1 = "C is fun"
-const String2 = "Python is cool"
-const String3 = "JavaScript is amazing"
-console.log(String1 + "\n" + String2 + "\n" + String3);
+const String1 = 'C is fun';
+const String2 = 'Python is cool';
+const String3 = 'JavaScript is amazing';
+console.log(String1 + '\n' + String2 + '\n' + String3);
