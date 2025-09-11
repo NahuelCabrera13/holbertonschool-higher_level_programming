dd897fe2120ba03f0d2339a0c917937927d437ba
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-09-11 09:22:48 -0300

    pepe

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    x_a = tuple_a + (0, 0)
-    x_b = tuple_b + (0, 0)
-
-    return (x_a[0] + x_b[0], x_a[1] + x_b[1])
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
