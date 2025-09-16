588ee4f50edfd77f8e77b6ecfe273d2d1158ddbf
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-09-16 13:21:34 -0300

    pepe

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """pepe"""
 
     def __init__(self, size=0):
-        if isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
