227686639f4fff5ebda917aa6c7c98259222ef22
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-09-17 13:50:52 -0300

    pepe

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -23,3 +23,10 @@ class Square:
 
     def area(self):
         return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
