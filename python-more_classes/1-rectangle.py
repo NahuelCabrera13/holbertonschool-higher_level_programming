76a87c2de95071a90d5740263fd5a509a97a48e2
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-09-18 10:36:06 -0300

    pepe

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""pepe"""
+
+
+class Rectangle:
+    """pepe"""
+    def __init__(self, width, heigth):
+        self.width = width
+        self.heigth = heigth
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        
+        self.__width = value
+        
+    @property
+    def heigth(self):
+        return self.__heigth
+    
+    @heigth.setter
+    def heigth(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        
+        self.__heigth = value
+    
