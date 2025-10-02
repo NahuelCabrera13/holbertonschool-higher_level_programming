aba3934a16a77f355c494dafb068cddd80dec826
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-10-02 09:21:01 -0300

    pepe

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""pepe"""
+
+
+def read_file(filename=""):
+    """pepe"""
+    with open(filename, "r", encoding='utf-8') as pepe:
+        contenido = pepe.read()
+        print(contenido)
