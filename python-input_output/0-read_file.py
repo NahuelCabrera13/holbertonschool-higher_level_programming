ef93736105d2f7ce9abd49f37e9a7f3ffd940195
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-10-02 09:26:20 -0300

    pepe

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """pepe"""
     with open(filename, "r", encoding='utf-8') as pepe:
-        contenido = pepe.read()
-        print(contenido)
+        print(pepe.read(), end="")
