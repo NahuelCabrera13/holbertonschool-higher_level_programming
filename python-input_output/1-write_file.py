680437a438ac7c85f25bb2b7a858ccece924bff1
NahuelCabrera13 <nahuelcabrera200513@gamil.com>
2025-10-02 09:49:07 -0300

    pepe

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """pepe"""
 
 
-def write_file(filename=""):
+def write_file(filename="", text=""):
     """pepe"""
     with open(filename, "w", encoding='utf-8') as pepe:
-        print(pepe.len(), end="")
+        return pepe.write(text)
