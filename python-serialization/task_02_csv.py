eb945cb5cd0c82f1058b3e4f2d81df287295e3e3
NahuelCabrera13 <nahuelcabrera200513@gmail.com>
2025-10-05 23:51:46 -0300

    pepe

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+pepe
+"""
+
+import csv
+import json
+import os
+
+def convert_csv_to_json(csv_filename):
+    """pepe
+    """
+    try:
+        if not os.path.exists(csv_filename):
+            with open('data.json', 'w') as json_file:
+                json.dump([], json_file)
+            return False
+
+        with open(csv_filename, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = list(csv_reader)
+
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+        
+        return True
+
+    except Exception as e:
+        with open('data.json', 'w') as json_file:
+            json.dump([], json_file)
+        return False
