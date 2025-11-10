3f8c73fa4da549353e6949c8a19b6502ade53bc9
NahuelCabrera13 <nahuelcabrera200513@gmail.com>
2025-11-09 23:21:27 -0300

    pepe

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const argsCount = process.argv.length - 2;
 
-if (args.length === 0) {
+if (argsCount === 0) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (argsCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
